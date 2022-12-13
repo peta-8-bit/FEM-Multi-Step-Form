e55158e7daab79420ac5c0beda23c999e537b24c
peta-8-bit <messimuhilan2000@gmail.com>
2022-12-13 12:57:12 +0530

    page nav done minus animation

--- app.js
+++ app.js
@@ -1,42 +1,95 @@
 let pageNo = 1;
 
-currentBtn = document.getElementById("one");
-console.log("CurrentBtn:" + currentBtn);
-currentBtn.classList.add("active-button");
+nextBtn = document.getElementById("next");
+backBtn = document.getElementById("back");
+p1 = document.querySelector(".p1");
+p2 = document.querySelector(".p2");
+p3 = document.querySelector(".p3");
+p4 = document.querySelector(".p4");
+
+document.getElementById("one").classList.add("active-button");
+p1.classList.remove("left");
+backBtn.classList.add("hide");
 
 nextPage = () => {
-  console.log("NEXTPAGE()");
+  console.log("nextPage()");
   if (pageNo < 4) {
-    displayPage();
-    pageNo += 1;
-    console.log(pageNo);
-  } else {
-    console.log("Over 4");
+    displayNextPage();
+  }
+  console.log("nextPageAfter()");
+};
+
+prevPage = () => {
+  if (pageNo > 1) {
+    displayPrevPage();
   }
 };
 
-displayPage = () => {
+displayNextPage = () => {
   switch (pageNo) {
     case 1: {
-      console.log("inside case1");
-      nextBtn = document.querySelector("#two");
-      nextBtn.classList.add("active-button");
+      pageNo += 1;
+      document.querySelector("#one").classList.remove("active-button");
+      document.querySelector("#two").classList.add("active-button");
+      backBtn.classList.remove("hide");
+
+      p1.classList.add("left");
+      p2.classList.remove("left");
       break;
     }
     case 2: {
-      console.log("inside case1");
-      nextBtn = document.querySelector("#three");
-      nextBtn.classList.add("active-button");
+      pageNo += 1;
+      document.querySelector("#two").classList.remove("active-button");
+      document.querySelector("#three").classList.add("active-button");
+
+      p2.classList.add("left");
+      p3.classList.remove("left");
       break;
     }
     case 3: {
-      console.log("inside case1");
-      nextBtn = document.querySelector("#four");
-      nextBtn.classList.add("active-button");
+      pageNo += 1;
+      document.querySelector("#three").classList.remove("active-button");
+      document.querySelector("#four").classList.add("active-button");
+
+      p3.classList.add("left");
+      p4.classList.remove("left");
+
+      nextBtn.classList.add("hide");
+      break;
+    }
+  }
+};
+
+displayPrevPage = () => {
+  switch (pageNo) {
+    case 2: {
+      pageNo -= 1;
+      document.querySelector("#two").classList.remove("active-button");
+      document.querySelector("#one").classList.add("active-button");
+      backBtn.classList.add("hide");
+
+      p1.classList.remove("left");
+      p2.classList.add("left");
+      break;
+    }
+    case 3: {
+      pageNo -= 1;
+      document.querySelector("#three").classList.remove("active-button");
+      document.querySelector("#two").classList.add("active-button");
+
+      p2.classList.remove("left");
+      p3.classList.add("left");
       break;
     }
     case 4: {
-      console.log("inside case1");
+      pageNo -= 1;
+      document.querySelector("#four").classList.remove("active-button");
+      document.querySelector("#three").classList.add("active-button");
+
+      p3.classList.remove("left");
+      p4.classList.add("left");
+
+      nextBtn.classList.remove("hide");
       break;
     }
   }
