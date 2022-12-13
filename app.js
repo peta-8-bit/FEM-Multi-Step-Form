438909ac665f5a3f9631ac2a002854c53a20a9b9
peta-8-bit <messimuhilan2000@gmail.com>
2022-12-13 20:00:58 +0530

    Improved moth year logic

--- app.js
+++ app.js
@@ -7,6 +7,23 @@ p2 = document.querySelector(".p2");
 p3 = document.querySelector(".p3");
 p4 = document.querySelector(".p4");
 
+addon1 = document.getElementById("online-service");
+addon2 = document.getElementById("larger-storage");
+addon3 = document.getElementById("customizable-profile");
+
+add1 = document.getElementById("addon-1");
+add2 = document.getElementById("addon-2");
+add3 = document.getElementById("addon-3");
+billFreq = document.getElementById("bill-frequency");
+billFreqAgain = document.getElementById("bill-frequency-again");
+s4Op1 = document.getElementById("s4-op1");
+s4Op2 = document.getElementById("s4-op2");
+s4Op3 = document.getElementById("s4-op3");
+s4Op4 = document.getElementById("s4-op4");
+s4Op5 = document.getElementById("s4-op5");
+val = document.querySelector("input[name='monthly-switch']").value;
+month = document.getElementById("monthly-switch");
+
 document.getElementById("one").classList.add("active-button");
 p1.classList.remove("left");
 backBtn.classList.add("hide");
@@ -94,3 +111,77 @@ displayPrevPage = () => {
     }
   }
 };
+
+monthly = () => {
+  if (month.checked) {
+    base = 90;
+    add1.textContent = "+10$/yr";
+    add2.textContent = "+20$/yr";
+    add3.textContent = "+20$/yr";
+
+    billFreq.textContent = "Yearly";
+    billFreqAgain.textContent = "year";
+
+    if (addon1.checked) {
+      base += 10;
+      document.querySelector(".OS").classList.remove("hide");
+    } else {
+      document.querySelector(".OS").classList.add("hide");
+    }
+    if (addon2.checked) {
+      base += 20;
+      document.querySelector(".LS").classList.remove("hide");
+    } else {
+      document.querySelector(".LS").classList.add("hide");
+    }
+    if (addon3.checked) {
+      base += 20;
+      document.querySelector(".CP").classList.remove("hide");
+    } else {
+      document.querySelector(".CP").classList.add("hide");
+    }
+
+    s4Op1.textContent = "$90/yr";
+    s4Op2.textContent = "+$10/yr";
+    s4Op3.textContent = "+$20/yr";
+    s4Op4.textContent = "+$20/yr";
+    s4Op5.textContent = `+$${base}yr`;
+  } else {
+    base = 9;
+    add1.textContent = "+1$/mo";
+    add2.textContent = "+2$/mo";
+    add3.textContent = "+2$/mo";
+
+    billFreq.textContent = "Monthly";
+    billFreqAgain.textContent = "month";
+
+    if (addon1.checked) {
+      base += 1;
+      document.querySelector(".OS").classList.remove("hide");
+    } else {
+      document.querySelector(".OS").classList.add("hide");
+    }
+    if (addon2.checked) {
+      base += 2;
+      document.querySelector(".LS").classList.remove("hide");
+    } else {
+      document.querySelector(".LS").classList.add("hide");
+    }
+    if (addon3.checked) {
+      base += 2;
+      document.querySelector(".CP").classList.remove("hide");
+    } else {
+      document.querySelector(".CP").classList.add("hide");
+    }
+
+    s4Op1.textContent = "$9/mo";
+    s4Op2.textContent = "+$1/mo";
+    s4Op3.textContent = "+$2/mo";
+    s4Op4.textContent = "+$2/mo";
+    s4Op5.textContent = `+$${base}/mo`;
+  }
+};
+
+monthly();
+
+//.Hide works but display none doesnot work
