ec55397c6563100651a7ce420f8eb083415c4b4c
peta-8-bit <messimuhilan2000@gmail.com>
2022-12-14 11:42:31 +0530

    This is killing meee

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ p2 = document.querySelector(".p2");
 p3 = document.querySelector(".p3");
 p4 = document.querySelector(".p4");
 
+s2Op1 = document.getElementById("s2Op1");
+s2Op2 = document.getElementById("s2Op2");
+s2Op3 = document.getElementById("s2Op3");
+
 addon1 = document.getElementById("online-service");
 addon2 = document.getElementById("larger-storage");
 addon3 = document.getElementById("customizable-profile");
@@ -14,6 +18,9 @@ addon3 = document.getElementById("customizable-profile");
 add1 = document.getElementById("addon-1");
 add2 = document.getElementById("addon-2");
 add3 = document.getElementById("addon-3");
+
+chosenOp = document.querySelectorAll("input[name='plan']");
+chosenName = document.getElementById("chosen-plan");
 billFreq = document.getElementById("bill-frequency");
 billFreqAgain = document.getElementById("bill-frequency-again");
 s4Op1 = document.getElementById("s4-op1");
@@ -113,14 +120,38 @@ displayPrevPage = () => {
 };
 
 monthly = () => {
+  let base, chosenPlan;
+  for (op of chosenOp) {
+    if (op.checked) {
+      chosenPlan = op.value;
+    }
+  }
+
   if (month.checked) {
-    base = 90;
-    add1.textContent = "+10$/yr";
-    add2.textContent = "+20$/yr";
-    add3.textContent = "+20$/yr";
+    add1.textContent = "+$10/yr";
+    add2.textContent = "+$20/yr";
+    add3.textContent = "+$20/yr";
+
+    s2Op1.textContent = "$90/yr";
+    s2Op2.textContent = "$120/yr";
+    s2Op3.textContent = "$150/yr";
 
     billFreq.textContent = "Yearly";
     billFreqAgain.textContent = "year";
+
+    if (chosenPlan === "Arcade") {
+      s4Op1.textContent = "$90/yr";
+      chosenName.textContent = "Arcade";
+      base = 90;
+    } else if (chosenPlan === "Advanced") {
+      s4Op1.textContent = "$120/yr";
+      chosenName.textContent = "Advanced";
+      base = 120;
+    } else {
+      s4Op1.textContent = "$150/yr";
+      chosenName.textContent = "Pro";
+      base = 150;
+    }
 
     if (addon1.checked) {
       base += 10;
@@ -141,19 +172,36 @@ monthly = () => {
       document.querySelector(".CP").classList.add("hide");
     }
 
-    s4Op1.textContent = "$90/yr";
     s4Op2.textContent = "+$10/yr";
     s4Op3.textContent = "+$20/yr";
     s4Op4.textContent = "+$20/yr";
     s4Op5.textContent = `+$${base}yr`;
   } else {
-    base = 9;
     add1.textContent = "+1$/mo";
     add2.textContent = "+2$/mo";
     add3.textContent = "+2$/mo";
 
+    s2Op1.textContent = "$9/mo";
+    s2Op2.textContent = "$12/mo";
+    s2Op3.textContent = "$15/mo";
+
     billFreq.textContent = "Monthly";
     billFreqAgain.textContent = "month";
+
+    if (chosenPlan === "Arcade") {
+      s4Op1.textContent = "$9/mo";
+      chosenName.textContent = "Arcade";
+
+      base = 9;
+    } else if (chosenPlan === "Advanced") {
+      s4Op1.textContent = "$12/mo";
+      chosenName.textContent = "Advanced";
+      base = 12;
+    } else {
+      s4Op1.textContent = "$15/mo";
+      chosenName.textContent = "Pro";
+      base = 15;
+    }
 
     if (addon1.checked) {
       base += 1;
@@ -174,7 +222,6 @@ monthly = () => {
       document.querySelector(".CP").classList.add("hide");
     }
 
-    s4Op1.textContent = "$9/mo";
     s4Op2.textContent = "+$1/mo";
     s4Op3.textContent = "+$2/mo";
     s4Op4.textContent = "+$2/mo";
@@ -185,3 +232,4 @@ monthly = () => {
 monthly();
 
 //.Hide works but display none doesnot work
+//Radio Button inputs stay selected even after refresh
